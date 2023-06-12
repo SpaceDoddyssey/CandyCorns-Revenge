e498259be98a11fff18ef90c6ef1a01dd9fdf6f7
Gigz <gigzpumpking@gmail.com>
2023-06-11 23:08:20 -0700

    Restart key added to pause menu

--- src/scenes/Pause.js
+++ src/scenes/Pause.js
@@ -10,9 +10,20 @@ class Pause extends Phaser.Scene {
             this.scene.resume('playScene').stop()
         })
 
+        let restartButton = new Button(centerX, centerY, '(R)estart', this, () => {
+            // .resume will start the update loop of the target scene again
+            // .stop will shutdown this scene, clear its display list, timers, etc.
+            this.scene.resume('playScene').stop();
+            var sceneRestart = this.scene.get('playScene');
+            this.sound.stopAll();
+            gameAudio = false;
+            sceneRestart.scene.restart();
+        })
+
         // input
         this.pauseKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
         keyFullscreen = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
+        this.restartKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
     }
 
     update() {
@@ -22,6 +33,14 @@ class Pause extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(this.pauseKey)) {
             // same as above
             this.scene.resume('playScene').stop()
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.restartKey)) {
+            // same as above
+            this.scene.resume('playScene').stop();
+            var sceneRestart = this.scene.get('playScene');
+            this.sound.stopAll();
+            gameAudio = false;
+            sceneRestart.scene.restart();
         }
     }
 }
